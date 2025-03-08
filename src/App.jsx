d14e014b0ffb5451bc3424d0a98ec4fe78de28e9
scasseca8 <sandiocasseca19@gmail.com>
2025-03-08 01:46:45 +0100

    Primeiro Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './Home'
+import TodoForm from './components/TodoForm'
+
+function App() {
+
+  return (
+    <div>
+      <Home />
+      <TodoForm />
+    </div>
+  )
+}
+
+export default App
