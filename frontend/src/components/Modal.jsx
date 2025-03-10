51c863d7513290ef5d6ad7bb7c3b0b41d9497123
scasseca8 <sandiocasseca19@gmail.com>
2025-03-10 20:57:12 +0100

    feact: Componente responsável de ser a Modal do TodoFrom

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,9 +1,33 @@
 import React, { useEffect } from "react";
 
-export default function Modal() {
+export default function Modal({ isOpen, onClose, children }) {
+  useEffect(() => {
+
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
+  const handleModalClick = (e) => {
+    e.stopPropagation();
+  };
+
+  if (!isOpen) return null;
 
   return (
-    <div className="modal-overlay">
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={handleModalClick}>
+        <button className="modal-close-btn" onClick={onClose}>
+          &times;
+        </button>
+        {children}
+      </div>
     </div>
   );
 }
