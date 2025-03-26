4672c4fe4082e934b7f66f75b96670db5d260b46
Santiago Casseca <157324675+scasseca8@users.noreply.github.com>
2025-03-26 23:53:30 +0100

    Update db.js

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 async function connectDataBase() {
     await mongoose
         .connect(
-            "mongodb+srv://scasseca:Santiago2024@cluster0.6dajq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+            "mongodb+srv://.6dajq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
         )
 }
 
